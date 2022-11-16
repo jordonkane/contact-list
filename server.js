411e3fe86f8e5fce922f42b1ad3121348f8b4b35
Jordon Kane <kanejord99@outlook.com>
2022-11-15 22:39:39 -0500

    update

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('ContactList')
+})
+
+app.listen(8000, () => {console.log('server running on port 8000')})
