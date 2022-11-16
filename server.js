d1be218293d31a0d049fc7d66af03c71b8d4ee44
Jordon Kane <kanejord99@outlook.com>
2022-11-16 10:48:10 -0500

    update

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyparser = require("body-parser")
+const path = require('path')
 
 const app = express();
 
+dotenv.config({path:'config.env'})
+const PORT = process.env.PORT || 8080
+
+//log request
+app.use(morgan('tiny'))
+
+//parse request
+app.use(bodyparser.urlencoded({extended:true}))
+
+//view engine
+app.set("view engine","ejs")
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+
 app.get('/', (req,res) => {
-    res.send('ContactList')
+    res.render('index')
 })
 
-app.listen(8000, () => {console.log('server running on port 8000')})
+app.listen(PORT, () => {console.log('server running on port 8000')})
